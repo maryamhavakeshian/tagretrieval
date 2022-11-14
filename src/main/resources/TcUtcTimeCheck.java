878a1aa2dfc1a6ea1dc095e9d4555443c07f6de8
Maryam Havakeshian <maryam.havakeshian@ericsson.com>
2022-11-14 18:43:17 -0500

    add AST and its corresponding source code

---
+++ src/main/resources/TcUtcTimeCheck.java
@@ -0,0 +1,12 @@
+package com.ericsson.msran.test.performance.tests;
+
+
+public class TcUtcTimeCheck  {
+
+@TestCaseId("0434ac9c-3459-4f37-8a8d-d28ef3f06599") 
+@JcatMethod(testTag = "BCI", testTitle = "tcUtcTimeCheck - Verifies setting of correct UTC time", testPriority = "")
+   public void afterTcUtcTimeCheck() {
+        logger.endLogStep();
+    }
+
+}
